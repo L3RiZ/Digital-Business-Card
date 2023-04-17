b04bb233e93f829df544d5545b5d485f24a249c4
L3RiZ <107563807+L3RiZ@users.noreply.github.com>
2023-04-17 17:41:02 +0200

    Info Section added

--- digital-business-card/src/components/Info.js
+++ digital-business-card/src/components/Info.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import portrait from '../images/sande.jpg'
 
 function Info(){
     return (
-        <h1>Info Section</h1>
+        <section className="info">
+            <img className='info--img' src={portrait} alt="myself" />
+            <h1>Steven Tornow</h1>
+            <h2>Frontend Developer</h2>
+            <p className='info--website'>steventornow.website</p>
+            <div className="info--links_cnt">
+                <a className='info--email' href="#"><i class="fa-solid fa-envelope"></i><p>Email</p></a>
+                <a className='info--linkedin' href="#"><i class="fa-brands fa-linkedin"></i><p>LinkedIn</p></a>
+            </div>
+        </section>
     )
 }
 
